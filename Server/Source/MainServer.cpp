801442fbc2436d2f6254b5b1a47cf1f9f5e353bf
uksuda <uksuda@naver.com>
2018-10-10 00:07:48 +0900

    first commit

--- Server/Source/MainServer.cpp
+++ Server/Source/MainServer.cpp
@@ -1,1 +1,32 @@
 #include "MainServer.h"
+
+MainServer::MainServer()
+{
+
+}
+
+MainServer::~MainServer()
+{
+
+}
+
+void MainServer::runServer()
+{
+
+}
+
+bool MainServer::initMainServer()
+{
+	return true;
+}
+
+MainServer* MainServer::createMainServer()
+{
+	MainServer* pServer = new MainServer;
+	if (pServer && pServer->initMainServer())
+	{
+		return pServer;
+	}
+
+	return nullptr;
+}
--- Server/Source/MainServer.h
+++ Server/Source/MainServer.h
@@ -1,9 +1,27 @@
 #ifndef __MAIN_SERVER_H__
 #define __MAIN_SERVER_H__
 
+
+class ServerSocket;
 class MainServer
 {
+private:
+	explicit MainServer();
+public:
+	~MainServer();
 
+public:
+	void runServer();
+
+private:
+	// ServerSocket
+	// WorkerThread
+
+private:
+	bool initMainServer();
+
+public:
+	static MainServer* createMainServer();
 };
 
 #endif
